e5b901441aa54137d281f86c60d4d2b43d32f0aa
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 11:36:58 +0200

    small blind

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,9 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import java.util.Map;
 
 public class Player {
 
@@ -11,9 +10,30 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
-        int smallBlind = json.get("small_blind").getAsInt();
 
-        return 0 + smallBlind * 2;
+        int dealer = json.get("dealer").getAsInt();
+        int small_blind_player = dealer + 1;
+        int big_blind_player = dealer + 2;
+        int we = 0;
+
+        JsonArray players = json.get("players").getAsJsonArray();
+        for (JsonElement player : players) {
+            JsonObject playerObject = player.getAsJsonObject();
+            if (playerObject.get("name").getAsString().equals("Gin")) {
+                we = playerObject.get("id").getAsInt();
+            }
+        }
+
+        int small_blind = json.get("small_blind").getAsInt();
+        int big_blind = small_blind * 2;
+
+        if (small_blind_player == we) {
+            return small_blind;
+        }
+        else if (big_blind_player == we) {
+            return big_blind;
+        }
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
