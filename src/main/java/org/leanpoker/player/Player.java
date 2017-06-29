74d89078546e7985625f863e0f48f18791d87ef4
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 13:47:17 +0200

    v12

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "11.00";
+    static final String VERSION = "12.00";
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
@@ -20,46 +20,46 @@ public class Player {
         GameVO gameVO = getGameVO(json);
 
 
+        PlayerVO currentPlayer = null;
+
         int currentBet = 0;
         List<PlayerVO> players = getPlayerVOList(json);
         for (PlayerVO player : players) {
             if (player.name.contains("Gin")) {
-
+                currentPlayer = player;
                 currentBet = player.bet;
                 currentBet = gameVO.current_buy_in - currentBet;
             }
         }
 
-        int small_blind = json.get("small_blind").getAsInt();
-        int big_blind = small_blind * 2;
-
-
-
-
-        JsonArray hole_cards = json.get("hole_cards").getAsJsonArray();
 
 
         int found = 0;
-        JsonObject card1 = hole_cards.get(0).getAsJsonObject();
-        JsonObject card2 = hole_cards.get(1).getAsJsonObject();
+        List<CardVO> hole_cards = currentPlayer.hole_cards;
+        if (hole_cards.size() >= 2) {
+            CardVO cardVO = currentPlayer.hole_cards.get(0);
+            CardVO cardVO1 = currentPlayer.hole_cards.get(1);
 
-        if (card1.get("rank").getAsString().equals(card2.get("rank").getAsString())) {
-            return Integer.MAX_VALUE;
-        }
-
-        for (JsonElement card: hole_cards) {
-            JsonObject cardObject = card.getAsJsonObject();
-            if (cardObject.get("rank").getAsString().equals("10") || cardObject.get("rank").getAsString().equals("J") || cardObject.get("rank").getAsString().equals("Q") || cardObject.get("rank").getAsString().equals("K")) {
-
-                found++;
+            if (cardVO.rank.equals(cardVO1.rank)) {
+                return Integer.MAX_VALUE;
             }
 
+        }
 
 
-        }
-        if (found == 2) {
-            return Integer.MAX_VALUE;
-        }
+//        for (JsonElement card: currentPlayer.hole_cards) {
+//            JsonObject cardObject = card.getAsJsonObject();
+//            if (cardObject.get("rank").getAsString().equals("10") || cardObject.get("rank").getAsString().equals("J") || cardObject.get("rank").getAsString().equals("Q") || cardObject.get("rank").getAsString().equals("K")) {
+//
+//                found++;
+//            }
+//
+//
+//
+//        }
+//        if (found == 2) {
+//            return Integer.MAX_VALUE;
+//        }
 
 
         return currentBet;
