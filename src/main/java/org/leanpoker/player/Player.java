8aa936f7a46e1e06bebb6be752bcdd6e8dd7b4ca
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 13:25:22 +0200

    v8

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,8 +9,6 @@ public class Player {
     static final String VERSION = "8.00";
 
     public static int betRequest(JsonElement request) {
-        return Integer.MAX_VALUE;
-        /*
         JsonObject json = request.getAsJsonObject();
 
         int dealer = json.get("dealer").getAsInt();
@@ -29,6 +27,34 @@ public class Player {
         int small_blind = json.get("small_blind").getAsInt();
         int big_blind = small_blind * 2;
 
+
+
+
+        JsonArray hole_cards = json.get("hole_cards").getAsJsonArray();
+
+
+        int found = 0;
+        JsonObject card1 = hole_cards.get(0).getAsJsonObject();
+        JsonObject card2 = hole_cards.get(1).getAsJsonObject();
+
+        if (card1.get("rank").getAsString().equals(card2.get("rank").getAsString())) {
+            return Integer.MAX_VALUE;
+        }
+
+        for (JsonElement card: hole_cards) {
+            JsonObject cardObject = card.getAsJsonObject();
+            if (cardObject.get("rank").getAsString().equals("10") || cardObject.get("rank").getAsString().equals("J") || cardObject.get("rank").getAsString().equals("Q") || cardObject.get("rank").getAsString().equals("K")) {
+
+                found++;
+            }
+
+
+
+        }
+        if (found == 2) {
+            return Integer.MAX_VALUE;
+        }
+
         if (small_blind_player == we) {
             return small_blind;
         }
@@ -36,25 +62,7 @@ public class Player {
             return big_blind;
         }
 
-        /*
-        JsonArray hole_cards = json.get("hole_cards").getAsJsonArray();
-
-
-        int found = 0;
-        for (JsonElement card: hole_cards) {
-            JsonObject cardObject = card.getAsJsonObject();
-            if (cardObject.get("rank").getAsString().equals("10") || cardObject.get("rank").getAsString().equals("J") || cardObject.get("rank").getAsString().equals("Q") || cardObject.get("rank").getAsString().equals("K")) {
-                found++;
-            }
-
-        }
-        if (found == 2) {
-
-        }
-
-        json.get("current_buy_in").getAsInt() - small_blind
-        */
-        //return 0;
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
