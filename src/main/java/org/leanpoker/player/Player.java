923d865aee3ed3a8fb871111610eb0ec8e7232fb
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 11:39:17 +0200

    small blind

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "1.00";
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
