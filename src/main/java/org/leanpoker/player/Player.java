4c0c194246fb64f9c5ab623a8989e4024ea88cb7
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 14:08:56 +0200

    v15

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,10 +12,53 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "14.00";
+    static final String VERSION = "15.00";
 
     public static int betRequest(JsonElement request) {
-        return Integer.MAX_VALUE;
+        JsonObject json = request.getAsJsonObject();
+
+        GameVO gameVO = getGameVO(json);
+
+
+        PlayerVO currentPlayer = null;
+
+        int currentBet = 0;
+        List<PlayerVO> players = gameVO.players;
+        for (PlayerVO player : players) {
+            if (player.name.contains("Gin")) {
+                currentPlayer = player;
+                currentBet = player.bet;
+                currentBet = gameVO.current_buy_in - currentBet;
+            }
+        }
+
+        int found = 0;
+        List<CardVO> hole_cards = currentPlayer.hole_cards;
+        if (hole_cards.size() >= 2) {
+            CardVO cardVO = currentPlayer.hole_cards.get(0);
+            CardVO cardVO1 = currentPlayer.hole_cards.get(1);
+
+            if (cardVO.rank.equals(cardVO1.rank)) {
+                return Integer.MAX_VALUE;
+            }
+
+        }
+
+
+        for (CardVO card: hole_cards) {
+            if (card.rank.equals("10") || card.rank.equals("J") || card.rank.equals("Q") || card.rank.equals("K")) {
+                found++;
+            }
+
+
+
+        }
+        if (found == 2) {
+            return Integer.MAX_VALUE;
+        }
+
+
+        return currentBet;
     }
     
     public static GameVO getGameVO(JsonElement request) {
@@ -24,5 +67,6 @@ public class Player {
     }
 
     public static void showdown(JsonElement game) {
+
     }
 }
