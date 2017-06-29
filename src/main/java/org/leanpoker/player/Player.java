ce17a2f474ce555471ca2589f5018993c5fba7c3
Graziano Felline <graziano.felline@gmail.com>
2017-06-29 14:38:16 +0200

    v22

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "21.00";
+    static final String VERSION = "22.00";
 
     public static int betRequest(JsonElement request) {
         JsonObject json = request.getAsJsonObject();
@@ -46,9 +46,9 @@ public class Player {
                         case Good:
                             return Integer.MAX_VALUE;
                         case Middle:
-                            return currentBet;
+                            return Integer.MAX_VALUE;
                         case Bad:
-                            return 0;
+                            return Integer.MAX_VALUE;
                     }
                 }
 
