5146ba10483d1e5a5c3c748beb4faf4f82abf584
Marco Niederberger <marco.niederberger@gmail.com>
2017-06-29 14:21:32 +0200

    Framework IV

--- src/main/java/org/leanpoker/player/StarthandEvaluator.java
+++ src/main/java/org/leanpoker/player/StarthandEvaluator.java
@@ -4,25 +4,58 @@ import java.util.List;
 
 public class StarthandEvaluator {
 	public enum StarthandQuality {
-		VeryGood
+		VeryGood,
+		Good,
+		Middle,
+		Bad
 	}
 	
-	
-	/*
-	 * Very good 
-	 * A & A
-	 * K & K
-	 * Q & Q
-	 * A & K
-	 * A & K
-	 */
-	
-	public void evaluate(List<CardVO> cardList) {
-		if (cardList.get(0).rank.equals("A") && cardList.get(1).rank.equals("A")) {
-			//StarthandQuality.VeryGood;
+	public StarthandQuality evaluate(List<CardVO> cardList) {
+		CardVO card_1 = cardList.get(0);
+		CardVO card_2 = cardList.get(0);
+		if (card_1.rank.equals("A") && card_2.rank.equals("A") 
+				|| card_1.rank.equals("K") && card_2.rank.equals("K")
+				|| card_1.rank.equals("Q") && card_2.rank.equals("Q")
+				|| card_1.rank.equals("A") && card_2.rank.equals("K")
+				|| card_1.rank.equals("A") && card_2.rank.equals("Q")) {
+			return StarthandQuality.VeryGood;
 		}
-		
-		
-		
+		else if (card_1.rank.equals("J") && card_2.rank.equals("J")
+				|| card_1.rank.equals("10") && card_2.rank.equals("10") 
+				|| card_1.rank.equals("9") && card_2.rank.equals("9")
+				|| card_1.rank.equals("8") && card_2.rank.equals("8")
+				|| card_1.rank.equals("A") && card_2.rank.equals("Q")
+				|| card_1.rank.equals("A") && card_2.rank.equals("J")
+				|| card_1.rank.equals("A") && card_2.rank.equals("10")
+				|| card_1.rank.equals("K") && card_2.rank.equals("Q")) {
+			return StarthandQuality.Good;
+		}
+		else if (card_1.rank.equals("7") && card_2.rank.equals("7")
+				|| card_1.rank.equals("6") && card_2.rank.equals("6") 
+				|| card_1.rank.equals("5") && card_2.rank.equals("5")
+				|| card_1.rank.equals("4") && card_2.rank.equals("4")
+				|| card_1.rank.equals("3") && card_2.rank.equals("3")
+				|| card_1.rank.equals("2") && card_2.rank.equals("2")
+				
+				|| card_1.rank.equals("A") && card_2.rank.equals("9")
+				|| card_1.rank.equals("A") && card_2.rank.equals("8")
+				|| card_1.rank.equals("A") && card_2.rank.equals("7")
+				|| card_1.rank.equals("A") && card_2.rank.equals("6")
+				|| card_1.rank.equals("A") && card_2.rank.equals("5")
+				|| card_1.rank.equals("A") && card_2.rank.equals("4")
+				|| card_1.rank.equals("A") && card_2.rank.equals("3")
+				|| card_1.rank.equals("A") && card_2.rank.equals("2")
+				
+				|| card_1.rank.equals("K") && card_2.rank.equals("J")
+				|| card_1.rank.equals("K") && card_2.rank.equals("10")
+				|| card_1.rank.equals("K") && card_2.rank.equals("9")
+				
+				|| card_1.rank.equals("Q") && card_2.rank.equals("10")
+				|| card_1.rank.equals("Q") && card_2.rank.equals("9")
+				
+				|| card_1.rank.equals("J") && card_2.rank.equals("10")) {
+			return StarthandQuality.Middle;
+		}
+		return StarthandQuality.Bad;
 	}
 }
